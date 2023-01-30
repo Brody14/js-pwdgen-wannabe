702988bdfb503ab5d6201a3e9092096242f580a7
Brody14 <stefania.spinas@gmail.com>
2023-01-30 15:19:59 +0100

    FEAT first prompt

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// richiamo l'elemento html
+
+const psd = document.getElementById('password');
+console.log(psd);
+
+//chiedere all'utente il suo nome
+
+let firstName;
+
+firstName = prompt('inserisci il tuo nome');
+console.log('il nome è', firstName);
+
+//chiedere all'utente il suo cognome
+
+//chiedere all'utente il suo colore preferito
+
+//chiedere all'utente un numero
